e16c8b0f5f6f4570f80b693ed5c35c44f5064bff
Yejin Cho <yejincho@usc.edu>
2025-04-21 13:48:44 -0700

    yejin: add ipapi.py

---
+++ imresearchutils/bgp/ipapi.py
@@ -0,0 +1,39 @@
+from ipaddress import ip_address
+import requests
+import pandas
+
+from imresearchutils.common import *
+
+
+class IPApiUtil:
+    def __init__(self,
+                 year: int = 2024,
+                 month: int = 1,
+                 data_dir: Path | None = None):
+        self.io_helper = IOHelper(self.__class__.__name__, data_dir=data_dir)
+
+        saved_file = IOHelper(module_name="", data_dir=Path("~/ssd-shared/PyASN")).module_dir / "ip_cache_asn_info.csv"
+        print(saved_file)
+        
+        if not saved_file.exists():
+            raise
+        
+        self.db = pandas.read_csv(
+            saved_file,
+            index_col=0,
+            low_memory=False,
+        ).to_dict(orient='index')
+        self.io_helper.logger.info(
+            f"Loaded IPApiUtil dataset from {saved_file}"
+        )
+    def get_asn_info(self, asn:int):
+        return self.db.get(asn, {})
+    
+    def is_mobile_proxy_hosting(self, asn):
+        info = self.get_asn_info(asn)
+        return (
+            bool(info.get("mobile", False)),
+            bool(info.get("proxy", False)),
+            bool(info.get("hosting", False)),
+        )
+    
