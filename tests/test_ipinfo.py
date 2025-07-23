84fc0265c496a1248fc4e280b9eb4128008f4e82
Yejin Cho <yejincho@usc.edu>
2025-07-23 13:29:42 -0700

    added IPInfoLiteUtil

--- tests/test_ipinfo.py
+++ tests/test_ipinfo.py
@@ -7,7 +7,6 @@ from ipaddress import IPv4Address
 
 @pytest.fixture(scope="session")
 def util():
-    # 이 fixture는 테스트 세션 전체에서 단 한 번만 실행됩니다.
     csv_path = Path("/home/yejin/ssd-data/DualStack/carrier-grade-NAT-investigation/ipinfo_lite-2.csv")
     util = IPInfoLiteUtil(saved_csv_path=csv_path)
     return util
