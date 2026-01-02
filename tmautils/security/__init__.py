b22ca324d2cbb33b5f96bb07c7d86f3c9df65b5b
Sulyab Thottungal Valapu <sulyabtv@gmail.com>
2026-01-02 15:57:30 -0800

    security: add get_cert_chain[_async]

--- tmautils/security/__init__.py
+++ tmautils/security/__init__.py
@@ -1,1 +1,4 @@
-from .cert import get_cert, get_cert_async
+from .cert import (
+    get_cert, get_cert_async,
+    get_cert_chain, get_cert_chain_async,
+)
--- tmautils/security/cert.py
+++ tmautils/security/cert.py
@@ -21,6 +21,31 @@ def _build_ctx(*, verify: bool, use_certifi: bool):
     return ctx
 
 
+def _get_cert_leaf_or_chain(
+    host: str | IPAddress,
+    port: int,
+    sni: Optional[str],
+    timeout: float,
+    verify: bool,
+    use_certifi: bool,
+    chain: bool,
+):
+    connect_host = str(host)
+    server_hostname = str(sni) if sni is not None else connect_host
+    ctx = _build_ctx(verify=verify, use_certifi=use_certifi)
+
+    with socket.create_connection((connect_host, port), timeout=timeout) as sock:
+        with ctx.wrap_socket(sock, server_hostname=server_hostname) as sslsock:
+            if chain:
+                der_list = sslsock.get_verified_chain()  # works for verify=False too
+                return [
+                    x509.load_der_x509_certificate(der) for der in der_list
+                ]
+            else:
+                der = sslsock.getpeercert(binary_form=True)
+                return x509.load_der_x509_certificate(der)
+
+
 def get_cert(
     host: str | IPAddress,
     port: int = 443,
@@ -46,7 +71,7 @@ def get_cert(
             Useful when connecting to an IP address but the certificate is for a domain name.
 
         timeout:
-            Connection timeout in seconds. Default is 5.0 seconds.
+            Connection timeout in seconds. Default is 8.0 seconds.
 
         verify:
             Whether to verify the server's TLS certificate. Default is True.
@@ -60,32 +85,73 @@ def get_cert(
         An `x509.Certificate` object representing the server's TLS certificate.
     """
 
-    connect_host = str(host)
-    server_hostname = str(sni) if sni is not None else connect_host
-    ctx = _build_ctx(verify=verify, use_certifi=use_certifi)
+    return _get_cert_leaf_or_chain(
+        host=host, port=port, sni=sni,
+        timeout=timeout,
+        verify=verify,
+        use_certifi=use_certifi,
+        chain=False,
+    )
 
-    with socket.create_connection((connect_host, port), timeout=timeout) as sock:
-        with ctx.wrap_socket(sock, server_hostname=server_hostname) as sslsock:
-            der = sslsock.getpeercert(binary_form=True)
-            return x509.load_der_x509_certificate(der)
 
-
-async def get_cert_async(
+def get_cert_chain(
     host: str | IPAddress,
     port: int = 443,
     *,
     sni: Optional[str] = None,
     timeout: float = 8.0,
-    ssl_handshake_timeout: float = 5.0,
     verify: bool = True,
     use_certifi: bool = True,
+) -> list[x509.Certificate]:
+    """
+    Retrieve the TLS certificate chain from a server.
+    See `get_cert_chain_async()` for async version.
+
+    Args:
+        host:
+            The hostname or IP address of the server.
+
+        port:
+            The port number to connect to. Default is 443.
+
+        sni:
+            The Server Name Indication (SNI) to use. If None, `host` is used.
+            Useful when connecting to an IP address but the certificate is for a domain name.
+
+        timeout:
+            Connection timeout in seconds. Default is 8.0 seconds.
+
+        verify:
+            Whether to verify the server's TLS certificate. Default is True.
+
+        use_certifi:
+            Whether to use the `certifi` CA bundle for verification. Default is True.
+            If False, the system's default CA bundle is used.
+            Ignored if `verify` is False.
+
+    Returns:
+        A list of `x509.Certificate` objects representing the server's TLS certificate chain.
+    """
+
+    return _get_cert_leaf_or_chain(
+        host=host, port=port, sni=sni,
+        timeout=timeout,
+        verify=verify,
+        use_certifi=use_certifi,
+        chain=True,
+    )
+
+
+async def _get_cert_leaf_or_chain_async(
+    host: str | IPAddress,
+    port: int,
+    sni: Optional[str],
+    timeout: float,
+    ssl_handshake_timeout: float,
+    verify: bool,
+    use_certifi: bool,
+    chain: bool,
 ):
-    """
-    Asynchronously retrieve the TLS certificate from a server.
-
-    See `get_cert()` (synchronous version) for details.
-    """
-
     connect_host = str(host)
     server_hostname = str(sni) if sni is not None else connect_host
     ctx = _build_ctx(verify=verify, use_certifi=use_certifi)
@@ -107,8 +173,15 @@ async def get_cert_async(
                 )
                 if sslobj is None:
                     raise RuntimeError("TLS handshake did not complete")
-                der = sslobj.getpeercert(binary_form=True)
-                return x509.load_der_x509_certificate(der)
+
+                if chain:
+                    der_list = sslobj.get_verified_chain()  # works for verify=False too
+                    return [
+                        x509.load_der_x509_certificate(der) for der in der_list
+                    ]
+                else:
+                    der = sslobj.getpeercert(binary_form=True)
+                    return x509.load_der_x509_certificate(der)
             finally:
                 if writer is not None:
                     writer.close()
@@ -116,5 +189,109 @@ async def get_cert_async(
                         await writer.wait_closed()
     except asyncio.TimeoutError:
         raise TimeoutError(
-            f"get_cert_async() for {host}:{port} timed out after {timeout}s"
+            f"get_cert_[chain_]async() for {host}:{port} timed out after {timeout}s"
         )
+
+
+async def get_cert_async(
+    host: str | IPAddress,
+    port: int = 443,
+    *,
+    sni: Optional[str] = None,
+    timeout: float = 8.0,
+    ssl_handshake_timeout: float = 5.0,
+    verify: bool = True,
+    use_certifi: bool = True,
+):
+    """
+    Asynchronously retrieve the TLS certificate from a server.
+
+    Args:
+        host:
+            The hostname or IP address of the server.
+
+        port:
+            The port number to connect to. Default is 443.
+
+        sni:
+            The Server Name Indication (SNI) to use. If None, `host` is used.
+            Useful when connecting to an IP address but the certificate is for a domain name.
+
+        timeout:
+            Connection timeout in seconds. Default is 8.0 seconds.
+
+        ssl_handshake_timeout:
+            Timeout for the SSL/TLS handshake in seconds. Default is 5.0 seconds.
+
+        verify:
+            Whether to verify the server's TLS certificate. Default is True.
+
+        use_certifi:
+            Whether to use the `certifi` CA bundle for verification. Default is True.
+            If False, the system's default CA bundle is used.
+            Ignored if `verify` is False.
+
+    Returns:
+        An `x509.Certificate` object representing the server's TLS certificate.
+    """
+
+    return await _get_cert_leaf_or_chain_async(
+        host=host, port=port, sni=sni,
+        timeout=timeout,
+        ssl_handshake_timeout=ssl_handshake_timeout,
+        verify=verify,
+        use_certifi=use_certifi,
+        chain=False,
+    )
+
+
+async def get_cert_chain_async(
+    host: str | IPAddress,
+    port: int = 443,
+    *,
+    sni: Optional[str] = None,
+    timeout: float = 8.0,
+    ssl_handshake_timeout: float = 5.0,
+    verify: bool = True,
+    use_certifi: bool = True,
+) -> list[x509.Certificate]:
+    """
+    Asynchronously retrieve the TLS certificate chain from a server.
+
+    Args:
+        host:
+            The hostname or IP address of the server.
+
+        port:
+            The port number to connect to. Default is 443.
+
+        sni:
+            The Server Name Indication (SNI) to use. If None, `host` is used.
+            Useful when connecting to an IP address but the certificate is for a domain name.
+
+        timeout:
+            Connection timeout in seconds. Default is 8.0 seconds.
+
+        ssl_handshake_timeout:
+            Timeout for the SSL/TLS handshake in seconds. Default is 5.0 seconds.
+
+        verify:
+            Whether to verify the server's TLS certificate. Default is True.
+
+        use_certifi:
+            Whether to use the `certifi` CA bundle for verification. Default is True.
+            If False, the system's default CA bundle is used.
+            Ignored if `verify` is False.
+
+    Returns:
+        A list of `x509.Certificate` objects representing the server's TLS certificate chain.
+    """
+
+    return await _get_cert_leaf_or_chain_async(
+        host=host, port=port, sni=sni,
+        timeout=timeout,
+        ssl_handshake_timeout=ssl_handshake_timeout,
+        verify=verify,
+        use_certifi=use_certifi,
+        chain=True,
+    )
