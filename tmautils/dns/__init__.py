01f41277b542fb1176bcb571b68fb7243161a5c4
Sulyab Thottungal Valapu <sulyabtv@gmail.com>
2026-01-12 18:22:40 -0800

    dns: add CzdsDownloadUtil
    meta: add pyjwt to dependencies

--- tmautils/dns/__init__.py
+++ tmautils/dns/__init__.py
@@ -1,3 +1,11 @@
 from .utils import dns_msg_semantic_hash
 from .dnspython import AsyncDnsPythonUtil
 from .openintel import OpenIntelZoneStreamUtil
+from .czds import CzdsDownloadUtil
+
+__all__ = [
+    "dns_msg_semantic_hash",
+    "AsyncDnsPythonUtil",
+    "OpenIntelZoneStreamUtil",
+    "CzdsDownloadUtil",
+]
